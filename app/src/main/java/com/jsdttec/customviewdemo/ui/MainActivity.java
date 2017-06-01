d546a8373744ab5b0ca491142ae9f7f11396d1ab
zhangzhongqiang <zhangzhongqiang@jsdttec.com>
2017-06-01 16:57:51 +0800

    CustomTextView1 Commit

--- app/src/main/java/com/jsdttec/customviewdemo/ui/MainActivity.java
+++ app/src/main/java/com/jsdttec/customviewdemo/ui/MainActivity.java
@@ -1,7 +1,8 @@
-package com.jsdttec.customviewdemo;
+package com.jsdttec.customviewdemo.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.jsdttec.customviewdemo.R;
 
 public class MainActivity extends AppCompatActivity {
 
