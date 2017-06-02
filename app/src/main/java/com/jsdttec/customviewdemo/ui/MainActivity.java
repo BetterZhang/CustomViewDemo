62482ac085acb92d1d86582134bffad96dfd6067
zhangzhongqiang <zhangzhongqiang@jsdttec.com>
2017-06-02 15:22:55 +0800

    Custom TopBar Commit

--- app/src/main/java/com/jsdttec/customviewdemo/ui/MainActivity.java
+++ app/src/main/java/com/jsdttec/customviewdemo/ui/MainActivity.java
@@ -1,14 +1,42 @@
 package com.jsdttec.customviewdemo.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 import com.jsdttec.customviewdemo.R;
+import com.jsdttec.customviewdemo.view.MyTopBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyTopBar mTopBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTopBar = (MyTopBar) findViewById(R.id.topBar);
+
+        mTopBar.setOnTopbarClickListener(new MyTopBar.topbarClickListener() {
+            @Override
+            public void leftClick() {
+                Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void rightClick() {
+                Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 控制TopBar上组件的状态
+        mTopBar.setButtonVisible(0, true);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mTopBar.setButtonVisible(1, false);
+            }
+        }, 10000);
     }
 }
